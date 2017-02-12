b8400a014360f26aa144a139b4d2d6326ac66f1d
David Obrite <david@ministrycentered.com>
2017-02-12 09:17:47 -0800

    feat(Backend): add render delegation using Renders Color

--- src/backend/backend.rs
+++ src/backend/backend.rs
@@ -1,4 +1,5 @@
 use yoga_wrapper;
+use renderable::Renderable;
 
 use super::renders::Renders;
 
@@ -7,4 +8,8 @@ pub trait Backend<'meas> {
 
     fn get_renderer(&mut self) -> &mut Self::Renderer;
     fn create_context<'text>(&'meas self, text: &'text str) -> yoga_wrapper::Context<'text, 'meas>;
+
+    fn render(&mut self, node: &Renderable<<<Self as Backend<'meas>>::Renderer as Renders>::Color>) {
+        self.get_renderer().render(node)
+    }
 }
