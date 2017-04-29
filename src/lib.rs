4fc0b18c48ea2f37b477408bb1dc22db7a10da6b
David Obrite <david@ministrycentered.com>
2017-04-29 13:18:34 -0700

    chore(deps): re-export yoga_wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-extern crate yoga_wrapper; // TODO should re-export all the style stuff (see rustbox example c)
+extern crate yoga_wrapper;
 
 mod backend;
 mod renderable;
@@ -6,10 +6,21 @@ pub mod style;
 mod text;
 mod view;
 
-
 pub use self::renderable::Renderable;
 pub use self::text::Text;
 pub use self::view::View;
 pub use backend::Backend;
 pub use backend::Builds;
 pub use backend::Renders;
+pub use yoga_wrapper::Align;
+pub use yoga_wrapper::Context;
+pub use yoga_wrapper::Dimensions;
+pub use yoga_wrapper::Direction;
+pub use yoga_wrapper::Edge;
+pub use yoga_wrapper::FlexDirection;
+pub use yoga_wrapper::FlexWrap;
+pub use yoga_wrapper::JustifyContent;
+pub use yoga_wrapper::Measures;
+pub use yoga_wrapper::Overflow;
+pub use yoga_wrapper::PositionType;
+pub use yoga_wrapper::Size;
